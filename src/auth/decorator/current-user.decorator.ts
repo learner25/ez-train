f06957b45d9ba552e80cdba392ebc91b162987a1
learner25 <saimon2999@gmail.com>
2025-12-07 16:29:10 +0600

    fixed enrollment IDOR issue

---
+++ src/auth/decorator/current-user.decorator.ts
@@ -0,0 +1,10 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const CurrentUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const req = ctx.switchToHttp().getRequest();
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
+    return req.user; // set by AuthGuard
+  },
+);
--- src/enrollment/enrollment.controller.ts
+++ src/enrollment/enrollment.controller.ts
@@ -1,20 +1,25 @@
-import { Controller, Post, Get, Body, Param, UseGuards } from '@nestjs/common';
+import { Controller, Post, Get, Body, UseGuards } from '@nestjs/common';
 import { EnrollmentService } from './enrollment.service';
 import { EnrollDto } from './dto/enroll.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { CurrentUser } from 'src/auth/decorator/current-user.decorator'; 
 
 @Controller('enrollments')
 @UseGuards(AuthGuard)
 export class EnrollmentController {
   constructor(private enrollmentService: EnrollmentService) {}
 
+  // Enroll authenticated user
   @Post()
-  enrollUser(@Body() body: EnrollDto) {
-    return this.enrollmentService.enroll(body);
+  enrollUser(@Body() body: Omit<EnrollDto, 'userId'>, @CurrentUser() user) {
+    console.log('Enrolling user:', user);
+    return this.enrollmentService.enroll({ ...body, userId: user.id });
   }
 
-  @Get(':userId')
-  getEnrollments(@Param('userId') userId: string) {
-    return this.enrollmentService.getUserEnrollments(userId);
+  // Get authenticated user's enrollments
+  @Get()
+  getEnrollments(@CurrentUser() user) {
+    // always return enrollments of the logged-in user
+    return this.enrollmentService.getUserEnrollments(user.id);
   }
 }
