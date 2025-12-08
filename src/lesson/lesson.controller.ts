e99cfd172634f3272921b393e5aebf8da937391e
Md. Saimon Islam <saimon2999@gmail.com>
2025-12-09 00:15:30 +0600

    Refactor lesson controller to remove token headers
    Removed x-session-token header from lesson controller methods.

--- src/lesson/lesson.controller.ts
+++ src/lesson/lesson.controller.ts
@@ -13,32 +13,33 @@ import { LessonService } from './lesson.service';
 import { CreateLessonInput } from './dto/create-lesson.input';
 import { UpdateLessonInput } from './dto/update-lesson.input';
 import { AuthGuard } from 'src/auth/auth.guard';
-
-@Controller('lessons')
+import { ApiBearerAuth } from '@nestjs/swagger';
+@ApiBearerAuth()
+@Controller('lessons') 
 @UseGuards(AuthGuard)
 export class LessonController {
   constructor(private readonly lessonService: LessonService) {}
   @Get()
-  getCourses(@Headers('x-session-token') token: string) {
+  getCourses() {
     return this.lessonService.getAllLessons();
   }
   @Post()
   create(
     @Body() data: CreateLessonInput,
-    @Headers('x-session-token') token: string,
+    
   ) {
     return this.lessonService.createLesson(data);
   }
 
   @Get(':id')
-  getOne(@Param('id') id: string, @Headers('x-session-token') token: string,) {
+  getOne(@Param('id') id: string) {
     return this.lessonService.getLessonById(Number(id));
   }
 
   @Get('course/:courseId')
   getByCourse(
     @Param('courseId') courseId: string,
-    @Headers('x-session-token') token: string,
+   
   ) {
     return this.lessonService.getLessonsByCourse(Number(courseId));
   }
@@ -47,13 +48,13 @@ export class LessonController {
   update(
     @Param('id') id: string,
     @Body() data: UpdateLessonInput,
-    @Headers('x-session-token') token: string,
+     
   ) {
     return this.lessonService.updateLesson(Number(id), data);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string, @Headers('x-session-token') token: string,) {
+  remove(@Param('id') id: string) {
     return this.lessonService.deleteLesson(Number(id));
   }
 }
