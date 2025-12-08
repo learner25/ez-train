ff030c0ce47970df4c59bc377598aa0af11c8c26
Md. Saimon Islam <saimon2999@gmail.com>
2025-12-09 00:14:29 +0600

    Refactor CourseController to remove token headers
    Removed x-session-token header requirement from course methods.

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -2,28 +2,31 @@ import { Controller, Get,Post,Body,Patch,Delete, Param, ParseIntPipe, UseGuards,
 import { CourseService } from './course.service';
 import { CreateCourseInput, UpdateCourseInput } from './dto/course.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
+
+@ApiBearerAuth()
 @Controller('courses')
+
 @UseGuards(AuthGuard)
 export class CourseController {
   constructor(private readonly courseService: CourseService) {}
 
   // Get all courses
   @Get()
-  @UseGuards(AuthGuard)
-  async getCourses(@Headers('x-session-token') token: string) {
+ 
+  //@ApiBearerAuth()
+  async getCourses() {
     return this.courseService.getAllCourses();
   }
 
   // Get a single course by ID
   @Get(':id')
-  async getCourseById(@Param('id', ParseIntPipe) id: number,
-    @Headers('x-session-token') token: string,
-  ) {
+   
+  async getCourseById(@Param('id', ParseIntPipe) id: number ) {
     return this.courseService.getCourseById(id);
   }
   @Post()
-  async createCourse(@Body() createCourseInput: CreateCourseInput,
-    @Headers('x-session-token') token: string,){
+  async createCourse(@Body() createCourseInput: CreateCourseInput){
     return this.courseService.create(createCourseInput);
   }
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
@@ -31,13 +34,12 @@ export class CourseController {
   update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateCourseInput: UpdateCourseInput,
-    @Headers('x-session-token') token: string,
+   
 ) {
   return this.courseService.update(id, updateCourseInput);
 }
  @Delete(':id')
-  async deleteCourse(@Param('id', ParseIntPipe) id: number,
-    @Headers('x-session-token') token: string,) {
+  async deleteCourse(@Param('id', ParseIntPipe) id: number,) {
     return this.courseService.delete(id);
   }
 }
