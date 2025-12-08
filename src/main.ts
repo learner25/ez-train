a143ddb1efd28781031c199c22e4bdbf18897810
Md. Saimon Islam <saimon2999@gmail.com>
2025-12-08 23:48:01 +0600

    Remove comments

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-// <-- REQUIRED FOR VERCEL -->
+ 
 import { Server } from 'http';
 
 let cachedServer: Server;
@@ -12,12 +12,12 @@ async function bootstrapServer() {
     logger: ['error', 'warn'], // optional: smaller logs for serverless
   });
 
-  // Enable CORS (Vercel needs this for frontend calls)
+ 
   app.enableCors({
     origin: '*',
   });
 
-  // Swagger configuration
+  
   const config = new DocumentBuilder()
     .setTitle('Japanse language API')
     .setDescription('Language learning API description')
@@ -47,11 +47,11 @@ async function bootstrapServer() {
 
   await app.init();
 
-  // convert nest app to Node HTTP server
+ 
   return app.getHttpAdapter().getHttpServer();
 }
 
-// 🟢 Export handler for Vercel
+ 
 export default async function handler(req: any, res: any) {
   if (!cachedServer) {
     cachedServer = await bootstrapServer();
